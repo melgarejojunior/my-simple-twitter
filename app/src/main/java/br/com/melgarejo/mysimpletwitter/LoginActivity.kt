208151af44f290bf70eeac50dd5859096499657c
Junior Melgarejo <juniormelgarejo@jera.com.br>
2019-05-21 22:54:54 -0400

    Initial commit: Activate databinding, created LoginActivity

---
+++ app/src/main/java/br/com/melgarejo/mysimpletwitter/LoginActivity.kt
@@ -0,0 +1,14 @@
+package br.com.melgarejo.mysimpletwitter
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+
+    }
+}
